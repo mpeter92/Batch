1d4250d5329100931621be1ad7987da73bc758bb
mpeter92 <mpeter92@yahoo.co.uk>
2016-06-09 17:32:54 +0100

    Add files via upload

---
+++ regquerySNMP.bat
@@ -0,0 +1,4 @@
+for /f "tokens=9 delims=^\" %%A in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Print\Monitors\Standard TCP/IP Port\Ports"')^
+do reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Print\Monitors\Standard TCP/IP Port\Ports\%%A" /v "SNMP Enabled"
+
+ping 127.0.0.1 -n 100
